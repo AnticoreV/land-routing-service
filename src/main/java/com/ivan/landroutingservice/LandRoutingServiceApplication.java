d48696e086eb5cbbcf9e1164dda23a34b908a315
ivan <sapronovivanprace@gmail.com>
2026-06-10 15:52:04 +0200

    stashed files added

--- src/main/java/com/ivan/landroutingservice/LandRoutingServiceApplication.java
+++ src/main/java/com/ivan/landroutingservice/LandRoutingServiceApplication.java
@@ -2,12 +2,13 @@ package com.ivan.landroutingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class LandRoutingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LandRoutingServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LandRoutingServiceApplication.class, args);
+    }
 }
--- src/test/java/com/ivan/landroutingservice/LandRoutingServiceApplicationTests.java
+++ src/test/java/com/ivan/landroutingservice/LandRoutingServiceApplicationTests.java
@@ -1,13 +1,33 @@
 package com.ivan.landroutingservice;
 
+import com.ivan.landroutingservice.providers.CountryGraphProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import java.util.Map;
+import java.util.Set;
 
 @SpringBootTest
 class LandRoutingServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        @Primary
+        CountryGraphProvider countryGraphProvider() {
+            return () -> Map.of(
+                    "CZE", Set.of("AUT"),
+                    "AUT", Set.of("CZE", "ITA"),
+                    "ITA", Set.of("AUT")
+            );
+        }
+    }
 }
